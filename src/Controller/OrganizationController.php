92fcc40cefaeba2f237a83fb70268478300ef408
BrandynL <brandyn.lordi@gmail.com>
2019-12-17 18:29:11 -0500

    add list controller

---
+++ src/Controller/OrganizationController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Organization;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class OrganizationController extends AbstractController {
+
+    /**
+     * @Route("/organizations/list", name="organizations_list")
+     */
+    public function showOrganizations(){
+        $organizations = $this->getDoctrine()->getRepository(Organization::class)->findAll();
+        return $this->render("organization/list.html.twig", [
+            'organizations' => $organizations
+        ])
+    }
+}
