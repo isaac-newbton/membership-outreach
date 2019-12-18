ab2eddbe4e0792e079c676c10b9d4f9597d318ab
BrandynL <brandyn.lordi@gmail.com>
2019-12-18 12:21:26 -0500

    create survey formtype

---
+++ src/Form/SurveyType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Survey;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SurveyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('dueDate')
+            ->add('surveyTemplate')
+            ->add('organization')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Survey::class,
+        ]);
+    }
+}
