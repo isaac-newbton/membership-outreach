1d23a9e2cdea12c56d6ed6d94a09c7d5c1b8194b
BrandynL <brandyn.lordi@gmail.com>
2019-12-19 12:36:28 -0500

    added base survey response functionality and controller

--- src/Controller/SurveyController.php
+++ src/Controller/SurveyController.php
@@ -2,10 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Question;
 use App\Entity\Survey;
+use App\Entity\SurveyResponse;
+use App\Form\SurveyResponseType;
 use App\Form\SurveyType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -47,6 +54,48 @@ class SurveyController extends AbstractController
         
         return $this->render("survey/form.html.twig", [
             "form" => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("surveys/{id}", name="surveys_response", requirements={"id"="\d+"})
+     */
+    public function surveyResponse(Request $request, Survey $survey){
+
+        $form = $this->createForm(SurveyResponseType::class, new SurveyResponse());
+        $form->remove('survey');
+        $form->remove('question');
+        $form->remove('answer');
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted()){
+            $entityManager = $this->getDoctrine()->getManager();
+            // get current all question ids
+            $surveyTemplate_questionIds = [];
+            foreach ($survey->getSurveyTemplate()->getQuestions() as $question){
+                $surveyTemplate_questionIds[] = $question->getId();
+            }
+            
+            $surveyResponse = new SurveyResponse();
+            $surveyResponse->setSurvey($survey);
+
+            foreach ($_POST as $k => $v) {
+                if (in_array($k, $surveyTemplate_questionIds)){
+                    $surveyResponse = new SurveyResponse();
+                    $surveyResponse->setSurvey($survey);
+                    $surveyResponse->setQuestion($this->getDoctrine()->getRepository(Question::class)->find($k));
+                    $surveyResponse->setAnswer($v);
+                    $entityManager->persist($surveyResponse);
+                }
+            }
+            $entityManager->flush();
+            return $this->redirectToRoute('surveys_list');
+        }
+
+        return $this->render("survey/form.html.twig", [
+            "form" => $form->createView(),
+            "survey" => $survey,
+            "questions" => $survey->getSurveyTemplate()->getQuestions(),
         ]);
     }
 }
