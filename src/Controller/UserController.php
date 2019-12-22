600e1894023313bf676c75ef36a6f75e2266a633
isaac-newbton <31673603+isaac-newbton@users.noreply.github.com>
2019-12-22 13:03:19 -0500

    user change password

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,21 +6,34 @@ use App\Form\ChangePasswordType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserController extends AbstractController{
 
 	/**
 	 * @Route("/account", name="account")
 	 */
-	public function account(Request $request){
-		$form = $this->createForm(ChangePasswordType::class, $this->getUser());
+	public function account(Request $request, UserPasswordEncoderInterface $encoder){
+		$form = $this->createForm(ChangePasswordType::class);
 
 		$form->handleRequest($request);
 		$form_data = $form->getData();
 		if($form->isSubmitted() && $form->isValid()){
-			$entityManager = $this->getDoctrine()->getManager();
+			// $user = $this->getUser();
+			// if($encoder->isPasswordValid($user, $form_data['password'])){
+			// 	if(6<=strlen($form_data['new_password'])){
+			// 		$entityManager = $this->getDoctrine()->getManager();
+			// 		$user->setPassword($encoder->encodePassword($user, $form_data['new_password']));
+			// 		$entityManager->persist($user);
+			// 		$entityManager->flush();
+			// 	}else{
+			// 		throw new \Exception('New password must be 6 characters or longer.');
+			// 	}
+			// }else{
+			// 	throw new \Exception('Current password invalid.');
+			// }
 		}
 
-		return $this->render("user/account.html.twig", ['form'=>$form, 'form_data'=>$form_data ?? false]);
+		return $this->render("user/account.html.twig", ['form'=>$form->createView(), 'form_data'=>$form_data ?? false]);
 	}
 }
--- src/Form/ChangePasswordType.php
+++ src/Form/ChangePasswordType.php
@@ -2,19 +2,20 @@
 
 namespace App\Form;
 
-use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\User;
 
 class ChangePasswordType extends AbstractType{
 	public function buildForm(FormBuilderInterface $builder, array $options){
 		$builder
 			->add('password', PasswordType::class, [
-				'mapped'=>false
+				'mapped'=>false,
+				'label'=>'Current Password'
 			])
 			->add('new_password', RepeatedType::class, [
 				'type'=>PasswordType::class,
