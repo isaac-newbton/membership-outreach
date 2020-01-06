b550a84c9b2c7873d370af3c6bee5eed589e9642
BrandynL <brandyn.lordi@gmail.com>
2020-01-06 11:21:58 -0500

    add edit and delete tag functionality

--- src/Controller/TagController.php
+++ src/Controller/TagController.php
@@ -5,7 +5,8 @@ use App\Entity\Tag;
 use App\Form\TagType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TagController extends AbstractController {
@@ -24,7 +25,7 @@ class TagController extends AbstractController {
 	/**
 	 * @Route("tags/add", name="tags_add")
 	 */
-	public function createTag(HttpFoundationRequest $request){
+	public function createTag(Request $request){
 		$form = $this->createForm(TagType::class);
 		$form->add('save', SubmitType::class);
 
@@ -42,5 +43,41 @@ class TagController extends AbstractController {
 		return $this->render("tags/form.html.twig", [
 			"form" => $form->createView()
 		]);
+	}
+
+	/**
+	 * @Route("tags/edit/{id}", name="tags_edit", requirements={"id":"\d+"})
+	 */
+	public function editTag(Request $request, Tag $tag){
+		if ($tag){
+			$form = $this->createForm(TagType::class, $tag);
+			$form->add('Update', SubmitType::class);
+			
+			$form->handleRequest($request);
+			if ($form->isSubmitted() && $form->isValid()){
+				$entityManager = $this->getDoctrine()->getManager();
+				$tag = $form->getData();
+				$entityManager->persist($tag);
+				$entityManager->flush();
+				return $this->redirectToRoute("tags_list");
+			}
+
+			return $this->render("tags/form.html.twig", [
+				"form" => $form->createView()
+			]);
+		}
+	}
+
+	/**
+	 * @Route("tags/delete/{id}", name="tags_delete", requirements={"id":"\d+"})
+	 */
+	public function deleteTag(Tag $tag, Session $session){
+		if ($tag){
+				$session->getFlashBag()->add('message', "Deleted tag: {$tag->getName()}");
+				$entityManager = $this->getDoctrine()->getManager();
+				$entityManager->remove($tag);
+				$entityManager->flush();
+				return $this->redirectToRoute("tags_list");
+		}
 	}
 }
